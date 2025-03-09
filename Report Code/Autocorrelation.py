a8b06c859af5b38fa27d3514717c051b1206ab9b
KPFS (Kasper Fibæk Schlosser) <KPFS@nne.com>
2025-03-09 16:25:58 +0100

    split into test and train

--- Report Code/Autocorrelation.py
+++ Report Code/Autocorrelation.py
@@ -61,16 +61,19 @@ def get_forecast(modelres, resids, N_step = 24, N_sim = 0, alpha = 0.1):
 
 
 
-def correction(actuals, est_quantiles, quant_est, model_type = "SARMA", N_step = 24, N_sim = 100, eval_resids = None):
+def correction(actuals, est_quantiles, quant_est, model_type = "SARMA", train_proportion = 0.5, N_step = 24, N_sim = 100, eval_resids = None):
     
     resids, _ = quant_est.transform(est_quantiles, actuals)
     resids = resids.squeeze()
     
+    n_train = int(len(resids)*train_proportion)
+    
     if model_type == "SARMA":
-        model = sm.tsa.SARIMAX(resids, order = (1,0,1), seasonal_order=(1,0,1,24))
+        model = sm.tsa.SARIMAX(resids[:n_train], order = (1,0,1), seasonal_order=(1,0,1,24))
     elif model_type == "ARMA":
-        model = sm.tsa.SARIMAX(resids, order = (1,0,1))
+        model = sm.tsa.SARIMAX(resids[:n_train], order = (1,0,1))
     model_res = model.fit()
+    test_res = model_res.apply(resids[n_train+1:], refit = False)
     
     normal_space = get_forecast(model_res, resids, N_step = N_step, N_sim = N_sim)
     
@@ -82,24 +85,29 @@ def correction(actuals, est_quantiles, quant_est, model_type = "SARMA", N_step =
         tmp = model_res.summary().tables[1].as_csv() 
         with open(save_path / (eval_resids + "_" + model_type + "_coefs.csv"), 'w') as f:
             f.write(tmp)
-                
-        pr = stats.norm().cdf(model_res.resid / np.sqrt(model_res.mse))
-        evaluation.evaluate_pseudoresids(pr[1:],
+        
+        pr = stats.norm().cdf(test_res.resid / np.sqrt(test_res.mse))
+        evaluation.evaluate_pseudoresids(pr,
                                    save_path = save_path,
                                    name = eval_resids + "_after",
                                    close_figs = True
                                    )
     
-    return normal_space, (cdf_forecast, cdf_sim), (orignal_forecast, original_sim)
+    return normal_space, (cdf_forecast, cdf_sim), (orignal_forecast, original_sim), n_train
 
 #%% load data
-#lets crash
 
 PATH = pathlib.Path()
 
 data = pd.read_pickle( PATH / "Data" / ("NABQR_results_full.pkl" ))
 zones = data.columns.get_level_values(level = 0).unique()
 quantiles = data[:]["Quantiles"].columns.unique().values
+
+#zone min and max
+#there seems to be a cap on production
+#set slighty lower and higher to be safe, there is a bit of noise
+zone_lims = {"DK1-offshore":(-1,1200),"DK1-onshore":(-1,3200),"DK2-offshore":(-1,1000),"DK2-onshore":(-1,550)}
+
 # there is some quantile crossings
 # i will try to fix by just sorting
 # might work?
@@ -109,17 +117,21 @@ for zone in zones:
 models = ["SARMA", "ARMA"]
 N_step = 24
 N_sim = 500
-    
-df_forecast = pd.DataFrame(index = data.index,
+train_proportion = 0.5
+n_train = int(len(data)*train_proportion)
+
+index = pd.MultiIndex.from_arrays([["train"]*n_train + ["test"]*(len(data) - n_train), data.index.values], names = ["dataset", "date"])
+
+df_forecast = pd.DataFrame(index = index,
                            columns = pd.MultiIndex.from_product([zones, ["nabqr"] + models, ("normal", "cdf", "original"), ("estimate", "lower prediction", "upper prediction")]),
                            dtype = np.float64
 )
 
-df_sim = pd.DataFrame(index = data.index,
+df_sim = pd.DataFrame(index = index,
                            columns = pd.MultiIndex.from_product([zones, ["nabqr"] + models, ("normal", "cdf", "original"), np.arange(N_sim)]),
                            dtype = np.float64
 )
-df_resids = pd.DataFrame(index = data.index,
+df_resids = pd.DataFrame(index = index,
                          columns = pd.MultiIndex.from_product([zones, ("normal", "cdf", "original")]),
                          dtype = np.float64
 )
@@ -127,31 +139,31 @@ df_resids = pd.DataFrame(index = data.index,
 #%% nabqr
 
 for par, val in zip(("estimate", "lower prediction", "upper prediction"), (0.5,0.05,0.95)):
-    df_forecast.loc[:, idx[:,"nabqr","normal", par]] = stats.norm().ppf(val)
+    df_forecast.loc[idx[:,:], idx[:,"nabqr","normal", par]] = stats.norm().ppf(val)
     
-df_sim.loc[:, idx[:, "nabqr", "normal", :]] = stats.norm().rvs((len(data), len(zones) * N_sim))
+df_sim.loc[idx[:,:], idx[:, "nabqr", "normal", :]] = stats.norm().rvs((len(data), len(zones) * N_sim))
 
 #convert to original space
 for zone in zones:
     actuals = data[zone,"Observed"].values.squeeze()
     est_quantiles = data[zone, "Quantiles"].values.squeeze()
-    quant_est = qm.piecewise_linear_model(quantiles, actuals.min() - 1, actuals.max()*1.1)
+    quant_est = qm.piecewise_linear_model(quantiles, *zone_lims[zone])
     
-    tmp = quant_est.back_transform(est_quantiles, df_forecast.loc[:, idx[zone,"nabqr","normal", :]])
-    df_forecast.loc[:, idx[zone,"nabqr","original", :]], df_forecast.loc[:, idx[zone,"nabqr","cdf", :]] = tmp
+    tmp = quant_est.back_transform(est_quantiles, df_forecast.loc[idx[:,:], idx[zone,"nabqr","normal", :]])
+    df_forecast.loc[idx[:,:], idx[zone,"nabqr","original", :]], df_forecast.loc[idx[:,:], idx[zone,"nabqr","cdf", :]] = tmp
     
-    tmp = quant_est.back_transform(est_quantiles, df_sim.loc[:, idx[zone,"nabqr","normal", :]])
-    df_sim.loc[:, idx[zone,"nabqr","original", :]], df_sim.loc[:, idx[zone,"nabqr","cdf", :]] = tmp
+    tmp = quant_est.back_transform(est_quantiles, df_sim.loc[idx[:,:], idx[zone,"nabqr","normal", :]])
+    df_sim.loc[idx[:,:], idx[zone,"nabqr","original", :]], df_sim.loc[idx[:,:], idx[zone,"nabqr","cdf", :]] = tmp
     
     #also fill resid while we are at it
     tmp = quant_est.transform(est_quantiles, actuals)
     
-    df_resids.loc[:, idx[zone, "original"]] = actuals
-    df_resids.loc[:, idx[zone, "normal"]] = tmp[0].squeeze()
-    df_resids.loc[:, idx[zone, "cdf"]] = tmp[1].squeeze()
+    df_resids.loc[idx[:,:], idx[zone, "original"]] = actuals
+    df_resids.loc[idx[:,:], idx[zone, "normal"]] = tmp[0].squeeze()
+    df_resids.loc[idx[:,:], idx[zone, "cdf"]] = tmp[1].squeeze()
     
     #evaluate resids
-    evaluation.evaluate_pseudoresids(df_resids.loc[:, idx[zone, "cdf"]].values.squeeze(),
+    evaluation.evaluate_pseudoresids(df_resids.loc[idx["test",:], idx[zone, "cdf"]].values.squeeze()[N_step:],
                                save_path = (PATH / "Figures" / "Correlation Structure" / "Residuals" ),
                                name = zone + "_before",
                                close_figs = True
@@ -164,38 +176,46 @@ for zone in zones:
         print(model)
         actuals = data[zone,"Observed"].values.squeeze()
         est_quantiles = data[zone, "Quantiles"].values.squeeze()
-        quant_est = qm.piecewise_linear_model(quantiles, actuals.min() - 1, actuals.max()*1.1)
+        quant_est = qm.piecewise_linear_model(quantiles, *zone_lims[zone])
         
-        normal, cdf, original = correction(actuals, est_quantiles, quant_est, model_type = model, N_step = N_step, N_sim = N_sim, eval_resids= zone + "_" + str(model) + "test")
+        normal, cdf, original, _ = correction(actuals, est_quantiles, quant_est,
+                                               model_type = model,
+                                               N_step = N_step, N_sim = N_sim,
+                                               train_proportion=train_proportion,
+                                               eval_resids= zone + "_" + str(model))
         
-        df_forecast.loc[:, idx[zone, model, "normal", :]] = normal[0]
-        df_forecast.loc[:, idx[zone, model, "cdf", :]] = cdf[0]
-        df_forecast.loc[:, idx[zone, model, "original", :]] = original[0]
+        df_forecast.loc[idx[:,:], idx[zone, model, "normal", :]] = normal[0]
+        df_forecast.loc[idx[:,:], idx[zone, model, "cdf", :]] = cdf[0]
+        df_forecast.loc[idx[:,:], idx[zone, model, "original", :]] = original[0]
         
-        df_sim.loc[:, idx[zone, model, "normal", :]] = normal[1]
-        df_sim.loc[:, idx[zone, model, "cdf", :]] = cdf[1]
-        df_sim.loc[:, idx[zone, model, "original", :]] = original[1]
+        df_sim.loc[idx[:,:], idx[zone, model, "normal", :]] = normal[1]
+        df_sim.loc[idx[:,:], idx[zone, model, "cdf", :]] = cdf[1]
+        df_sim.loc[idx[:,:], idx[zone, model, "original", :]] = original[1]
         
 
 #%% scores
 df_scores = pd.DataFrame(index = ["nabqr"] + list(models),
-                         columns = pd.MultiIndex.from_product([zones,["MAE", "MSE", "VARS", "CRPS"]]),
+                         columns = pd.MultiIndex.from_product([["train", "test"], zones, ["MAE", "MSE", "VARS", "CRPS"]]),
                          dtype = float)
 
-for zone in zones:
-    print(zone)
-    #weights = evaluation.variogram_weight(df_sim.loc[:, idx[zone, "nabqr", "original", :]].values)
+
+for dataset in ["train", "test"]:
+    print(dataset)
     
-    for p in df_scores.index:
-        print(p)
-        actuals = df_resids.loc[:,idx[zone, "original"]].values.squeeze()
-        predicted = df_forecast.loc[:,idx[zone, p, "original", "estimate"]].values
-        sim = df_sim.loc[:, idx[zone, p, "original", :]]
+    for zone in zones:
+        print(zone)
+        #weights = evaluation.variogram_weight(df_sim.loc[idx[:,:], idx[zone, "nabqr", "original", :]].values)
         
-        #scores = evaluation.calc_scores(actuals, predicted, sim.values, VARS_kwargs = {"weights": weights})
-        scores = evaluation.calc_scores(actuals, predicted, sim.values)
-        
-        df_scores.loc[p, idx[zone, :]] = scores
+        for p in df_scores.index:
+            print(p)
+            actuals = df_resids.loc[idx[dataset,:],idx[zone, "original"]].values.squeeze()[N_step:]
+            predicted = df_forecast.loc[idx[dataset,:],idx[zone, p, "original", "estimate"]].values[N_step:]
+            sim = df_sim.loc[idx[dataset,:], idx[zone, p, "original", :]].values[N_step:,:]
+            
+            #scores = evaluation.calc_scores(actuals, predicted, sim.values, VARS_kwargs = {"weights": weights})
+            scores = evaluation.calc_scores(actuals, predicted, sim)
+            
+            df_scores.loc[p, idx[dataset, zone, :]] = scores
 
 
 #%% forecasts in orignal space
@@ -243,7 +263,7 @@ plots = [
 def line_plots(zone, space, xlims, ylims, N_step = 24, save = True, Name = None, plot_smaller = False):
     # function for making this specific plot
     # not general, just saves a lot of writing
-    x = df_forecast.index
+    x = df_forecast.index.get_level_values("date")
     
     fig, ax = plt.subplots(figsize = (14,8), layout = "tight")
     sns.scatterplot(x = x, y = df_resids[zone, space], color = 'black', marker = 'x', ax = ax)
@@ -265,11 +285,18 @@ def line_plots(zone, space, xlims, ylims, N_step = 24, save = True, Name = None,
         alpha = 0.3
     )
     ax.vlines(
-        data.index[N_step-1::N_step],
+        index.get_level_values("date")[N_step-1::N_step],
         np.nanmin(df_forecast[zone].values),
         np.nanmax(df_forecast[zone].values),
         color = 'black',
         linestyle = '--'
+    )
+    ax.vlines(
+        index.get_level_values("date")[n_train],
+        np.nanmin(df_forecast[zone].values),
+        np.nanmax(df_forecast[zone].values),
+        color = 'black',
+        linestyle = '-'
     )
     ax.set_title(zone)
     ax.set_ylabel("Production")
@@ -348,22 +375,25 @@ for zone, xlim, ylim in zip(zones, xlims, ylims):
     abp_plot(zone, "normal", (-3,3), (-3,3))
     
 
-#%% scores 
-tmp = df_scores.T.reset_index(names = ["zone", "metric"]).melt(value_vars = ["nabqr", "SARMA", "ARMA"],
-                                                               var_name = "model",
-                                                               value_name ="score",
-                                                               id_vars = ["zone","metric"])
-fig = sns.catplot(tmp,
-                x = "model", y = "score", hue = "model",
-                row = "metric", col = "zone", kind = "bar",
-                palette = {"nabqr": "navy", "SARMA":"crimson", "ARMA":"olivedrab"},
-                height = 3, aspect = 1.2,
-                sharey = False, sharex=False,
-                margin_titles = True,
-                dodge = False)
-fig.tight_layout()
+#%% scores
 
-fig.savefig( (PATH / "Figures" / "Correlation Structure" / "scores.png" ))
+for dataset in ["train", "test"]:
+    tmp = df_scores.xs(dataset, axis = 1).T.reset_index(names = ["zone", "metric"]).melt(
+        value_vars = ["nabqr", "SARMA", "ARMA"],
+        var_name = "model",
+        value_name ="score",
+        id_vars = ["zone","metric"])
+    fig = sns.catplot(tmp,
+                    x = "model", y = "score", hue = "model",
+                    row = "metric", col = "zone", kind = "bar",
+                    palette = {"nabqr": "navy", "SARMA":"crimson", "ARMA":"olivedrab"},
+                    height = 3, aspect = 1.2,
+                    sharey = False, sharex=False,
+                    margin_titles = True,
+                    dodge = False)
+    fig.tight_layout()
+    
+    fig.savefig( (PATH / "Figures" / "Correlation Structure" / (dataset + "_scores.png" )))
 
 
 #%% variogram
@@ -376,18 +406,18 @@ c_range = []
 for i, zone in enumerate(zones):
     for j, p in enumerate(df_scores.index):
         
-        actuals = data[zone,"Observed"].values.squeeze()
-        sim = df_sim.loc[:, idx[zone, p, "original", :]]
+        actuals = df_resids.loc[idx["test",:], idx[zone, "original"]].values[N_step:]
+        sim = df_sim.loc[idx["test",:], idx[zone, p, "original", :]].values[N_step:,:]
         
-        score, variogram = evaluation.variogram_score(sim.values, actuals)
+        score, variogram = evaluation.variogram_score(sim, actuals)
         #variogram = np.log(variogram)
         np.fill_diagonal(variogram, 0)
         
         if p == "nabqr": c_range.append((variogram.min(),variogram.max()))
         
-        
         cbar = False
         if j == len(df_scores.index)-1: cbar = True
+        
         sns.heatmap(variogram,
                     ax = axes[i,j],
                     vmin = c_range[i][0], vmax = c_range[i][1],
