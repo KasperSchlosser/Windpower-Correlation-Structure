ee03e69928df8063d0cd33c9b149fe47931f2861
KPFS (Kasper Fibæk Schlosser) <KPFS@nne.com>
2025-04-17 21:30:51 +0200

    data results done

--- Code/Data code/Data.py
+++ Code/Data code/Data.py
@@ -2,6 +2,7 @@
 # should be run sparingly as fitting the lstm take quite a while
 
 import pandas as pd
+import tomllib
 from pathlib import Path
 
 def cleantz(ensemble):
@@ -15,15 +16,17 @@ def cleantz(ensemble):
     
     return ensemble
 
-PATH = Path.cwd()
-load_path = PATH.parents[1] / "Data" / "Raw Data" 
-save_path = PATH.parents[1] / "Data" / "Data"
+PATH = Path.cwd().parents[1]
+load_path = PATH / "Data" / "Raw Data" 
+save_path = PATH / "Data" / "Data"
 
 # The observed production sometimes goes negative, this is an error
 # values of 0 would also give problems
 # this value is for practical purpose 0
 low_value = 0.01
 
+with open(PATH / "Settings" / "zone limits.toml", "rb") as f:
+    zone_lims = tomllib.load(f)
 
 #%% Load Data
 # Finds ensembles and observations
@@ -35,6 +38,9 @@ raw_ensembles = {}
 raw_observations = {}
 cleaned_ensembles =  {}
 cleaned_observations = {}
+normalised_ensembles = {}
+normalised_observations = {}
+
 for ens, obs in zip(ensembles, observations):
     
     zone = "-".join(ens.stem.split("_")[:2])
@@ -51,18 +57,24 @@ for ens, obs in zip(ensembles, observations):
     #remove tz from ensemble and drop the missing hours from observation
     ens = cleantz(ens)
     
-    # remove nan and change problematic observations
-    obs = obs.dropna()
+    # change problematic observations
     obs[obs < low_value] = low_value
 
     cleaned_ensembles[zone] = ens
     cleaned_observations[zone] = obs
+    
+    normalised_ensembles[zone] = (ens - ens.min()) / (ens.max() - ens.min())
+    normalised_observations[zone] = (obs - zone_lims[zone][0]) / (zone_lims[zone][1] - zone_lims[zone][0])
 
-# Save data
 cleaned_ensembles = pd.concat(cleaned_ensembles, axis = 1, keys = cleaned_ensembles.keys())
 cleaned_observations = pd.concat(cleaned_observations, axis = 1, keys = cleaned_observations.keys())
+cleaned_observations.dropna(inplace = True)
+
 raw_ensembles = pd.concat(raw_ensembles, axis = 1, keys = raw_ensembles.keys())
 raw_observations = pd.concat(raw_observations, axis = 1, keys = raw_observations.keys())
+
+normalised_ensembles = pd.concat(normalised_ensembles, axis = 1, keys = normalised_ensembles.keys())
+normalised_observations = pd.concat(normalised_observations, axis = 1, keys = normalised_observations.keys())
 
 cleaned_ensembles.to_pickle(save_path / "cleaned_ensembles.pkl")
 cleaned_ensembles.to_csv(save_path / "cleaned_ensembles.csv")
@@ -75,3 +87,9 @@ raw_ensembles.to_csv(save_path / "raw_ensembles.csv")
 
 raw_observations.to_pickle(save_path / "raw_observations.pkl")
 raw_observations.to_csv(save_path / "raw_observations.csv")
+
+normalised_ensembles.to_pickle(save_path / "normalised_ensembles.pkl")
+normalised_ensembles.to_csv(save_path / "normalised_ensembles.csv")
+
+normalised_observations.to_pickle(save_path / "normalised_observations.pkl")
+normalised_observations.to_csv(save_path / "normalised_observations.csv")
--- Code/Figure Code/Data.py
+++ Code/Figure Code/Data.py
@@ -1,6 +1,7 @@
 import pathlib
 import pandas as pd
 import numpy as np
+import tomllib
 
 import matplotlib.pyplot as plt
 
@@ -18,6 +19,10 @@ clean_ens = pd.read_pickle(load_path / "cleaned_ensembles.pkl")
 
 zones = raw_obs.columns.unique(level = 0)
 index = raw_obs.index
+small_plot = [np.datetime64("2022-09-01"), np.datetime64("2022-09-14")]
+
+with open(PATH / "Settings" / "zone limits.toml", "rb") as f:
+    zone_lims = tomllib.load(f)
 
 #%% illustrate ensembles
 # this takes a long time to run
@@ -44,7 +49,7 @@ fig.suptitle("Ensembles")
 fig.savefig(save_path / "Figures" / "Ensembles full")
 
 #Smaller view
-axes[0].set_xlim([np.datetime64("2022-08-29"), np.datetime64("2022-09-12")])
+axes[0].set_xlim(small_plot)
 fig.savefig(save_path / "Figures" / "Ensembles small")
 
 
@@ -58,13 +63,6 @@ ax.set_yticks([])
 ax.tick_params("x", rotation = 15)
 
 lines = []
-lims = {
-    "DK1-offshore": [0,1250],
-    "DK1-onshore": [0,3600],
-    "DK2-offshore": [0,1000],
-    "DK2-onshore": [0,650],
-    }
-
 for zone, color, offset in zip(zones, plt.rcParams['axes.prop_cycle'].by_key()["color"], [0, -0.05,-0.1,-0.15] ):
 
     twin = ax.twinx()
@@ -78,12 +76,12 @@ for zone, color, offset in zip(zones, plt.rcParams['axes.prop_cycle'].by_key()["
     twin.yaxis.set_ticks_position('left')
     twin.tick_params(axis = "y", which = "both", colors = color)
     
-    twin.set_ylim(*lims[zone])
+    twin.set_ylim(*zone_lims[zone])
     
     twin.set_ylabel(zone, labelpad = -1, color = color)
     
 fig.savefig(save_path / "Figures" / "Observations full")
-ax.set_xlim([np.datetime64("2022-08-29"), np.datetime64("2022-09-12")])
+ax.set_xlim(small_plot)
 fig.savefig(save_path / "Figures" / "Observations small")
 
 #%% comparison ensembles, observations
@@ -103,6 +101,55 @@ for zone, color, ax in zip(zones, plt.rcParams['axes.prop_cycle'].by_key()["colo
 fig.supxlabel("Date")
 fig.supylabel("Power production (kW)")
 fig.suptitle("Ensembles vs Observations")
-axes[0].set_xlim([np.datetime64("2022-09-01"), np.datetime64("2022-09-14")])
+axes[0].set_xlim(small_plot)
 
 fig.savefig(save_path / "Figures" / "Comparison")
+
+#%% tables
+
+sum_table = pd.DataFrame(index = ["Observations", "Ensembles"])
+sum_table["Number before data cleaning"] = (len(raw_obs), len(raw_ens))
+sum_table["Number after data cleaning"] = (len(clean_obs), len(clean_ens))
+sum_table["Number of Features"] = (np.nan, len(clean_ens.columns.unique(level = 1)))
+sum_table["First entry"] = (clean_obs.index.min(), clean_ens.index.min())
+sum_table["Last entry"] = (clean_obs.index.max(), clean_ens.index.max())
+
+sum_table.style.to_latex(save_path / "Tables" / "Summary.tex",
+                         position = "h",
+                         label = "data:table:summary",
+                         caption = (
+                             (
+                                 'Summary table for data. Data cleaning involed stripping timezone information,'
+                                 'dropping nan-values, and setting negative values to a low value (0.01)'),
+                             "Summary table of data"
+                             ),
+                         hrules = True)
+
+ens_table = pd.DataFrame(index = raw_ens.columns.unique(level = 0))
+
+ens_table["Minimum"] = raw_ens.T.groupby(level = 0).min().min(axis = 1)
+ens_table["Maximum"] = raw_ens.T.groupby(level = 0).max().max(axis = 1)
+ens_table["Missing values"] = raw_ens.isna().T.groupby(level = 0).sum().sum(axis = 1)
+ens_table["Infinite values"] = ((raw_ens == np.inf) | (raw_ens == -np.inf)).T.groupby(level = 0).sum().sum(axis = 1)
+
+ens_table.style.to_latex(save_path / "Tables" / "Ensembles.tex",
+                         position = "h",
+                         label = "data:table:ensembles",
+                         caption = ('Summary table for ensembles',"Summary table of data"),
+                         hrules = True)
+
+obs_table = pd.DataFrame(index = raw_obs.columns.unique(level = 0))
+
+obs_table["Minimum"] = raw_obs.T.groupby(level = 0).min().min(axis = 1)
+obs_table["Maximum"] = raw_obs.T.groupby(level = 0).max().max(axis = 1)
+obs_table["Negative observations"] = (raw_obs < 0).T.groupby(level = 0).sum().sum(axis = 1)
+obs_table["Problematic observations"] = ((raw_obs > 0) & (raw_obs < 0.01)).T.groupby(level = 0).sum().sum(axis = 1)
+obs_table["Missing values"] = raw_obs.isna().T.groupby(level = 0).sum().sum(axis = 1)
+obs_table["Infinite values"] = ((raw_obs == np.inf) | (raw_obs == -np.inf)).T.groupby(level = 0).sum().sum(axis = 1)
+
+obs_table.style.to_latex(save_path / "Tables" / "Observations.tex",
+                         position = "h",
+                         label = "data:table:observations",
+                         caption = ('Summary table for observations. Problematic values are are values very close to 0, $0 \leq x < 0.01$',
+                                    "Summary table of data"),
+                         hrules = True)
